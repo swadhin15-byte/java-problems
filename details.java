7f1e6f82646c71ff7e222622151bc12ab85d17c2
swadhin1800 <swadhinpatro15@gmail.com>
2025-09-05 12:02:23 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,14 @@
+public class Exercise {
+
+  public static void main(String[] args){
+      
+       // write code here
+       
+       int age = 25;
+       double height = 5.9;
+       String name = "Alex";
+       
+       System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height);
+
+  }
+}
