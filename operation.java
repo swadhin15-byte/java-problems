d348002d6163c2dc512dff75e168852a75e9238e
swadhin1800 <swadhinpatro15@gmail.com>
2025-09-08 22:29:00 +0530

    Add files via upload

---
+++ operation.java
@@ -0,0 +1,22 @@
+public class Exercise {
+    public static void main(String[] args){
+     
+         // write code here
+         int a = 20;
+         int b = 10;
+         
+         
+         boolean result = a > b;
+         boolean result2 = a > b && b > 0;
+         
+         System.out.println("Addition: "  +  (a + b));
+         System.out.println("Subtraction: "  +  (a - b));
+         System.out.println("Multiplication: "  +  (a * b));
+         System.out.println("Division: "  +  (a / b));
+         System.out.println("Is a greater than b? "  +  result);
+         System.out.println("Is a > b and b > 0? "  +  result2);
+         
+    
+         
+    }
+}
